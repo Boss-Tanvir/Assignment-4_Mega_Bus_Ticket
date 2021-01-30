6e2bc98e3f904eb52a111efc760f95e9f2136d22
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2021-01-30 23:23:37 +0600

    Third Commit

--- main.js
+++ main.js
@@ -1,94 +1,33 @@
-// // Fist class event Handaler
-
-// document.getElementById('first_class_increase').addEventListener('click', function () {
-//     let firstClassInput = document.getElementById('first_class_input_count')
-//     let firstClassNumber = parseInt(firstClassInput.value)
-//     let firstClassNumberCount = firstClassNumber + 1
-//     firstClassInput.value = firstClassNumberCount   
-//     let firstClassTotal = firstClassNumberCount * 150
-//     document.getElementById('totalPrice').innerText = firstClassTotal
-// })
-
-
-
-// document.getElementById('first_class_decrease').addEventListener('click', function () {
-//     let firstClassInput = document.getElementById('first_class_input_count')
-//     let firstClassNumber = parseInt(firstClassInput.value)
-//     let firstClassNumberCount = firstClassNumber - 1
-//     firstClassInput.value = firstClassNumberCount   
-//     let firstClassTotal = firstClassNumberCount * 150
-//     document.getElementById('totalPrice').innerText = firstClassTotal
-// })
-
 
 document.getElementById('first_class_increase').addEventListener('click', function () {
-ticketRate(true)
+    firstClassticketRate(true)
 })
 
 document.getElementById('first_class_decrease').addEventListener('click', function () {
-    ticketRate(false)
+    firstClassticketRate(false)
 })
 
 
-
-
-
-
-function ticketRate(isIncrease) {
+function firstClassticketRate(isIncrease) {
 
         let firstClassInput = document.getElementById('first_class_input_count')
         let firstClassNumber = parseInt(firstClassInput.value)
         let firstClassNumberCount = firstClassNumber
         if (isIncrease == true) {
-            firstClassNumberCount = firstClassNumber + 1
-            
+            firstClassNumberCount = firstClassNumber + 1          
         }
 if(isIncrease == false && firstClassNumber > 0){
     firstClassNumberCount = firstClassNumber - 1
 }
-          
-        // let firstClassNumberCount = firstClassNumber - 1
+                 
         firstClassInput.value = firstClassNumberCount   
         let firstClassTotal = firstClassNumberCount * 150
        
         document.getElementById('totalPrice').innerText = firstClassTotal
-
         calculateTotal()
     
 }
 
-
-
-// document.getElementById('economy_class_increase').addEventListener('click', function () {
-//     ticketRate2(true)
-//     })
-    
-//     document.getElementById('economy_class_dcrease').addEventListener('click', function () {
-//         ticketRate2(false)
-//     })
-
-
-
-// function ticketRate2(isIncrease) {
-
-//     let firstClassInput = document.getElementById('economy_class_input_count')
-//     let firstClassNumber = parseInt(firstClassInput.value)
-//     let firstClassNumberCount = firstClassNumber
-//     if (isIncrease == true) {
-//         firstClassNumberCount = firstClassNumber + 1
-        
-//     }
-// if(isIncrease == false && firstClassNumber > 0){
-// firstClassNumberCount = firstClassNumber - 1
-// }
-      
-//     // let firstClassNumberCount = firstClassNumber - 1
-//     firstClassInput.value = firstClassNumberCount   
-//     let firstClassTotal = firstClassNumberCount * 100
-   
-//     document.getElementById('totalprice').innerHTML = firstClassTotal
-
-// }
 
 
 
@@ -96,38 +35,38 @@ if(isIncrease == false && firstClassNumber > 0){
 
 
 document.getElementById('economy_class_increase').addEventListener('click', function () {
-    let firstClassInput = document.getElementById('economy_class_input_count')
-    let firstClassNumber = parseInt(firstClassInput.value)
-    let firstClassNumberCount2 = firstClassNumber + 1
-    firstClassInput.value = firstClassNumberCount2  
-    let firstClassTotal = firstClassNumberCount2 * 100
-    document.getElementById('totalPrice').innerText = firstClassTotal
+    let economyClassInput = document.getElementById('economy_class_input_count')
+    let economyClassNumber = parseInt(economyClassInput.value)
+    let economyClassNumberCount2 = economyClassNumber + 1
+    economyClassInput.value = economyClassNumberCount2  
+    let economyClassTotal = economyClassNumberCount2 * 100
+    document.getElementById('totalPrice').innerText = economyClassTotal
 
     calculateTotal()
 })
 
 
 document.getElementById('economy_class_dcrease').addEventListener('click', function () {
-    let firstClassInput = document.getElementById('economy_class_input_count')
-    let firstClassNumber = parseInt(firstClassInput.value)
-    let firstClassNumberCount2= firstClassNumber - 1
-    firstClassInput.value = firstClassNumberCount2  
-    let firstClassTotal = firstClassNumberCount2 * 100
-    document.getElementById('totalPrice').innerText = firstClassTotal
+    let economyClassInput = document.getElementById('economy_class_input_count')
+    let economyClassNumber = parseInt(economyClassInput.value)
+    let economyClassNumberCount2= economyClassNumber - 1
+    economyClassInput.value = economyClassNumberCount2  
+    let economyClassTotal = economyClassNumberCount2 * 100
+    document.getElementById('totalPrice').innerText = economyClassTotal
 
     calculateTotal()
 })
 
 
 function calculateTotal() {
-    let firstClassCost = document.getElementById('first_class_input_count')
-    let firstClassCostNumber = parseInt(firstClassCost.value)
+    let allClassCost = document.getElementById('first_class_input_count')
+    let allClassCostNumber = parseInt(allClassCost.value)
 
     let ecomomyClassCost = document.getElementById('economy_class_input_count')
     let economyClassCostNumber = parseInt(ecomomyClassCost.value)
 
 
-    let total = firstClassCostNumber * 150 + economyClassCostNumber * 100
+    let total = allClassCostNumber * 150 + economyClassCostNumber * 100
 
     document.getElementById('totalPrice').innerText = total
 
